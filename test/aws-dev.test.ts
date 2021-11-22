032b79182a7fb7925dda20397e22cb4cebeb0f12
Mayowa Oseni <mayowa.oseni@gentrack.com>
2021-11-22 13:49:14 +0000

    Initial commit

---
+++ test/aws-dev.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AwsDev from '../lib/aws-dev-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-dev-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsDev.AwsDevStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
